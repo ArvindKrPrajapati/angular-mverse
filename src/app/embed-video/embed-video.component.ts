dd859845845124472413c2887fb3cacc5a307677
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-11-15 23:29:11 +0530

    Added gdrive server

--- src/app/embed-video/embed-video.component.ts
+++ src/app/embed-video/embed-video.component.ts
@@ -23,6 +23,8 @@ export class EmbedVideoComponent implements OnInit {
   type: string = "movie";
   e_loading: boolean = true;
   episodes: any;
+  active = "vidsrc"
+  tv_imdb = ""
   constructor(private _route: ActivatedRoute, private _http: HttpClient, public sanitizer: DomSanitizer) {
     _route.paramMap.subscribe((p: any) => {
       this.id = p.get("id")
@@ -30,6 +32,7 @@ export class EmbedVideoComponent implements OnInit {
         this.type = "tv"
         this.s = p.get("s")
         this.e = p.get("e")
+        this.active = "vidsrc"
         let src = "https://vidsrc.me/embed/" + this.id + "/" + this.s + "-" + this.e
         this.src = this.sanitizer.bypassSecurityTrustResourceUrl(src);
         this.fetchEpisodes()
@@ -55,6 +58,15 @@ export class EmbedVideoComponent implements OnInit {
       }, (err: any) => {
         // alert(this.type)
       })
+    if (this.type == "tv") {
+      this._http.get(this.url + "tv/" + this.id + "/external_ids", { headers: new HttpHeaders({ "Authorization": this.token }) })
+        .subscribe((res: any) => {
+          this.tv_imdb = res.imdb_id;
+
+        }, (err: any) => {
+          // alert(this.type)
+        })
+    }
   }
   fetchEpisodes() {
     this.e_loading = true
@@ -65,6 +77,29 @@ export class EmbedVideoComponent implements OnInit {
         this.e_loading = false
       })
   }
+  changeServer(name: string) {
+    switch (name) {
+      case "gdrive":
+        this.active = "gdrive"
+        let src = "http://database.gdriveplayer.us/player.php?imdb="
+        if (this.type == "tv") {
+          src += this.tv_imdb + "&type=series&season=" + this.s + "&episode=" + this.e
+          this.src = this.sanitizer.bypassSecurityTrustResourceUrl(src);
+        } else {
+          this.src = this.sanitizer.bypassSecurityTrustResourceUrl(src + this.data.imdb_id);
+        }
+        break;
 
+      default:
+        this.active = "vidsrc"
+        let s = "https://vidsrc.me/embed/"
+        if (this.type == "tv") {
+          this.src = this.sanitizer.bypassSecurityTrustResourceUrl(s + this.id + "/" + this.s + "-" + this.e);
+        } else {
+          this.src = this.sanitizer.bypassSecurityTrustResourceUrl(s + this.id);
+        }
+        break;
+    }
+  }
 
 }
